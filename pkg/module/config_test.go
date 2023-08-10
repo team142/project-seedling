14544c32ae890f052700cc9874717c20778834e1
bjorn <8081834+s41nn0n@users.noreply.github.com>
2023-08-10 07:52:46 +0200

    tests

--- pkg/module/config_test.go
+++ pkg/module/config_test.go
@@ -135,3 +135,216 @@ func TestConfig_getFileDir(t *testing.T) {
 		})
 	}
 }
+
+func Test_validateFinalCharacter(t *testing.T) {
+	type args struct {
+		base string
+		sep  string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "Simple Test",
+			args: args{
+				base: "",
+				sep:  "/",
+			},
+			want: "/",
+		},
+		{
+			name: "Simple Test With Ending",
+			args: args{
+				base: "/",
+				sep:  "/",
+			},
+			want: "/",
+		},
+		{
+			name: "Simple Test with simple base",
+			args: args{
+				base: "base",
+				sep:  "/",
+			},
+			want: "base/",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := validateFinalCharacter(tt.args.base, tt.args.sep); got != tt.want {
+				t.Errorf("validateFinalCharacter() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestConfig_GetAPIPath(t *testing.T) {
+	type fields struct {
+		Version string
+		APIPath string
+	}
+	type args struct {
+		base string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   string
+	}{
+		{
+			name: "Simple Test",
+			fields: fields{
+				Version: "",
+				APIPath: "",
+			},
+			args: args{
+				base: "",
+			},
+			want: "/",
+		},
+		{
+			name: "Simple Test with base",
+			fields: fields{
+				Version: "",
+				APIPath: "",
+			},
+			args: args{
+				base: "base",
+			},
+			want: "/base",
+		},
+		{
+			name: "Simple Test With API Path",
+			fields: fields{
+				Version: "",
+				APIPath: "APIPath",
+			},
+			args: args{
+				base: "",
+			},
+			want: "/APIPath",
+		},
+		{
+			name: "Simple Test With API Path and version",
+			fields: fields{
+				Version: "V1",
+				APIPath: "APIPath",
+			},
+			args: args{
+				base: "",
+			},
+			want: "/APIPath/v1",
+		},
+		{
+			name: "Simple Test With API Path, version and base",
+			fields: fields{
+				Version: "V1",
+				APIPath: "APIPath",
+			},
+			args: args{
+				base: "base",
+			},
+			want: "/APIPath/v1/base",
+		},
+		{
+			name: "Simple Test With API Path, version and base",
+			fields: fields{
+				Version: "V1",
+				APIPath: "/APIPath/v1/test",
+			},
+			args: args{
+				base: "base",
+			},
+			want: "/APIPath/v1/test/base",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Config{
+				Version: tt.fields.Version,
+				APIPath: tt.fields.APIPath,
+			}
+			if got := c.GetAPIPath(tt.args.base); got != tt.want {
+				t.Errorf("GetAPIPath() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_getAPIPath(t *testing.T) {
+	type args struct {
+		start string
+		end   string
+		sep   string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "Simple",
+			args: args{
+				start: "",
+				end:   "",
+				sep:   "/",
+			},
+			want: "/",
+		},
+		{
+			name: "Simple No Duplicate",
+			args: args{
+				start: "/",
+				end:   "",
+				sep:   "/",
+			},
+			want: "/",
+		},
+		{
+			name: "Simple start",
+			args: args{
+				start: "start",
+				end:   "",
+				sep:   "/",
+			},
+			want: "/start",
+		},
+		{
+			name: "Simple start and end",
+			args: args{
+				start: "start",
+				end:   "end",
+				sep:   "/",
+			},
+			want: "/start/end",
+		},
+		{
+			name: "Simple sep for start and end",
+			args: args{
+				start: "/",
+				end:   "end",
+				sep:   "/",
+			},
+			want: "/end",
+		},
+		{
+			name: "Simple API Path test with version",
+			args: args{
+				start: "/APIPath",
+				end:   "v1",
+				sep:   "/",
+			},
+			want: "/APIPath/v1",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getAPIPath(tt.args.start, tt.args.end, tt.args.sep); got != tt.want {
+				t.Errorf("getAPIPath() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
