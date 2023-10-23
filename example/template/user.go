6b87962851f95ade801ac65b85afd8f35a1741cf
bjorn <8081834+s41nn0n@users.noreply.github.com>
2023-10-23 06:44:15 +0200

    refactor

--- example/template/user.go
+++ example/template/user.go
@@ -1,7 +1,5 @@
-//go:generate go run ../../cmd/template/v1/main.go -i user.go -t template
-package basic
-
-import "database/sql"
+//go:generate go run ../../cmd/template -t template
+package template
 
 // User is a basic user of the system
 // @DiscoverFunction
@@ -14,46 +12,6 @@ type User struct {
 	FirstName string `json:"first_name,omitempty"` // FirstName fn
 	LastName  string `json:"last_name,omitempty"`  // LastName ln
 	CreatedAt string `json:"-"`
-}
-
-func (u *User) GetMultiple(db *sql.DB, params map[string]string) (error, []User, int, int, int64) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (u *User) Delete(db *sql.DB) error {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (u *User) Save(db *sql.DB, override bool) (error, bool) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (u *User) Validate() error {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (u *User) GetDeleteStatement() (error, string) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (u *User) GetSelectStatement(params map[string]string) (error, string) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (u *User) GetUpdateStatement() (error, string) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (u *User) GetCreateStatement() (error, string) {
-	//TODO implement me
-	panic("implement me")
 }
 
 // UserIgnored should be ignored during the generation
